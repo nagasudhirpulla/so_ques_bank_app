e4ed0bc40786c251cc6df3590a9878cbaac3683c
Nagasudhir <nagasudhirpulla@gmail.com>
2025-03-14 00:10:06 +0530

    2024 ques bank start

---
+++ basic_2024/questions.js
@@ -0,0 +1,506 @@
+var questions = [
+  {
+    "Topic": "Sub-Station & Bus Bar Schemes",
+    "Question": "Which of the following substation layouts sectionalizes the bus so that the entire substation is not shutdown in the event of a bus fault?",
+    "Option1": "Single bus with tie breaker, ring bus",
+    "Option2": "One and half breaker",
+    "Option3": "Double bus double breaker",
+    "Option4": "Main and\n Transfer",
+    "answer": 1
+  },
+  {
+    "Topic": "Sub-Station & Bus Bar Schemes",
+    "Question": "The winding of the transformer which may be used for protection as well as for serving load is called the ________ winding",
+    "Option1": "Primary",
+    "Option2": "Secondary",
+    "Option3": "Tertiary",
+    "Option4": "Auxiliary",
+    "answer": 3
+  },
+  {
+    "Topic": "Sub-Station & Bus Bar Schemes",
+    "Question": "What type of transformer is designed so that the primary and secondary windings are connected together?",
+    "Option1": "Three winding transformer",
+    "Option2": "Two winding transformer",
+    "Option3": "Auto transformer",
+    "Option4": "Converter transformer",
+    "answer": 3
+  },
+  {
+    "Topic": "Sub-Station & Bus Bar Schemes",
+    "Question": "What is certainly not available in a switching station ? Choose the best option",
+    "Option1": "Transformer",
+    "Option2": "Bus Reactor",
+    "Option3": "Line Reactor",
+    "Option4": "Series Capacitor",
+    "answer": 1
+  },
+  {
+    "Topic": "Sub-Station & Bus Bar Schemes",
+    "Question": "Which switching scheme does not require a bus coupler?",
+    "Option1": "Double Main with transfer",
+    "Option2": "Double Bus",
+    "Option3": "Single Bus and Transfer",
+    "Option4": "Single Bus with sectionalizer",
+    "answer": 4
+  },
+  {
+    "Topic": "Sub-Station & Bus Bar Schemes",
+    "Question": "Which switching arrangement does not require a separate bus bar protection?",
+    "Option1": "Double bus",
+    "Option2": "Ring bus",
+    "Option3": "One and half breaker",
+    "Option4": "Double Main with transfer",
+    "answer": 2
+  },
+  {
+    "Topic": "Sub-Station & Bus Bar Schemes",
+    "Question": "Which is the most widely used switching scheme at 400 kV level",
+    "Option1": "Ring bus",
+    "Option2": "Single Bus and Transfer",
+    "Option3": "One and half breaker",
+    "Option4": "Double bus",
+    "answer": 3
+  },
+  {
+    "Topic": "Sub-Station & Bus Bar Schemes",
+    "Question": "In a 400 kV substation having a breaker and a half scheme, how many breakers would have to be tripped to de energize a line which shares the dia with a ICT?",
+    "Option1": 1.5,
+    "Option2": 3,
+    "Option3": 1,
+    "Option4": 2,
+    "answer": 4
+  },
+  {
+    "Topic": "Sub-Station & Bus Bar Schemes",
+    "Question": "Which is the first equipment that would be encountered as one enters a substation (say 400 kV) from the line end?",
+    "Option1": "Line CT",
+    "Option2": "Wavetrap",
+    "Option3": "Line CVT",
+    "Option4": "Lightening Arrestor",
+    "answer": 4
+  },
+  {
+    "Topic": "Sub-Station & Bus Bar Schemes",
+    "Question": "Which is likely to be the most costly equipment in a substation?",
+    "Option1": "Inter Connecting Transformer",
+    "Option2": "Line Reactor",
+    "Option3": "CT",
+    "Option4": "Circuit Breaker",
+    "answer": 1
+  },
+  {
+    "Topic": "Sub-Station & Bus Bar Schemes",
+    "Question": "Bus Couplers are provided  to:",
+    "Option1": "Tie different buses",
+    "Option2": "Segregate bus into\n number of sections",
+    "Option3": "Isolate fault in bus",
+    "Option4": "Synchronize",
+    "answer": 1
+  },
+  {
+    "Topic": "Sub-Station & Bus Bar Schemes",
+    "Question": "Instrument transformers are necessary",
+    "Option1": "To operate \nprotective relays",
+    "Option2": "To record the current and voltages",
+    "Option3": "To provide proportional current and voltage to relays, meters  and synchronizing",
+    "Option4": "To act as isolating device in high voltage system",
+    "answer": 3
+  },
+  {
+    "Topic": "Sub-Station & Bus Bar Schemes",
+    "Question": "Surge arresters are provided to",
+    "Option1": "Limit the fault \ncurrent",
+    "Option2": "To limit the voltage",
+    "Option3": "To divert lightning surges to ground",
+    "Option4": "To absorb switching and lighting surges and discharge",
+    "answer": 4
+  },
+  {
+    "Topic": "Sub-Station & Bus Bar Schemes",
+    "Question": "In a substation all non current carrying parts shall be connected to",
+    "Option1": "Adjacent \nequipment",
+    "Option2": "Separate Earth \nelectrodes",
+    "Option3": "Earth mat",
+    "Option4": "Need not be earthed",
+    "answer": 3
+  },
+  {
+    "Topic": "Sub-Station & Bus Bar Schemes",
+    "Question": "Earth Potential rise can \nbe controlled by",
+    "Option1": "Reducing the system voltage",
+    "Option2": "Increasing the \nearth resistance",
+    "Option3": "Increasing the area of Substation",
+    "Option4": "Reducing the fault current and the earth resistance",
+    "answer": 4
+  },
+  {
+    "Topic": "LEGAL_REGULATORY_FRAMEWORK",
+    "Question": "Judgment of the appellate tribunal can be appealed against in:",
+    "Option1": "Sessions Court",
+    "Option2": "High Court",
+    "Option3": "Supreme Court",
+    "Option4": "Consumer court",
+    "answer": 3
+  },
+  {
+    "Topic": "LEGAL_REGULATORY_FRAMEWORK",
+    "Question": "Cross subsidy is that part of electricity cost realized from ___",
+    "Option1": "High End Consumer",
+    "Option2": "Government",
+    "Option3": "Public",
+    "Option4": "Generation Company",
+    "answer": 1
+  },
+  {
+    "Topic": "LEGAL_REGULATORY_FRAMEWORK",
+    "Question": "Appeal  from the order of the State Commission or the Central Commission can be preferred before which authority?",
+    "Option1": "Disctrict court",
+    "Option2": "Appellete Tribunal",
+    "Option3": "High Court",
+    "Option4": "Supreme \nCourt",
+    "answer": 2
+  },
+  {
+    "Topic": "LEGAL_REGULATORY_FRAMEWORK",
+    "Question": "What is the time limitation for filing  Appeal before the Appellate  Tribunal?",
+    "Option1": "30 DAYS",
+    "Option2": "60 DAYS",
+    "Option3": "90 DAYS",
+    "Option4": "45 DAYS",
+    "answer": 4
+  },
+  {
+    "Topic": "LEGAL_REGULATORY_FRAMEWORK",
+    "Question": "Electricity Act, 2003  was promulgated after repealing which earlier enactment of Electricity Laws",
+    "Option1": "Electtricity Act, 1910",
+    "Option2": "Electricity Supply Act, 1948",
+    "Option3": "The Electricity Regulatory  Commssions Act, 1998",
+    "Option4": "All of these",
+    "answer": 4
+  },
+  {
+    "Topic": "LEGAL_REGULATORY_FRAMEWORK",
+    "Question": "What are the activities that are licensed under the new Elecitiricty Act, 2003 ?",
+    "Option1": "Generation",
+    "Option2": "Transmission",
+    "Option3": "Distribution",
+    "Option4": "Both Transmission and Distribution",
+    "answer": 4
+  },
+  {
+    "Topic": "LEGAL_REGULATORY_FRAMEWORK",
+    "Question": "Under which extraordinary circumstances is the appropriate government empowered to direct  a genarating company to operate and maintain any generating station in accordance to their directions",
+    "Option1": "a natural disaster/ calamity",
+    "Option2": "threat to security of the state",
+    "Option3": "any circumstance arising in public interst",
+    "Option4": "all of these options",
+    "answer": 4
+  },
+  {
+    "Topic": "LEGAL_REGULATORY_FRAMEWORK",
+    "Question": "The National Electricity Policy and the National Tariff Policy is prepared by the",
+    "Option1": "Central Government",
+    "Option2": "CERC",
+    "Option3": "CEA",
+    "Option4": "Central Government in consultation with the state governments and the CEA",
+    "answer": 4
+  },
+  {
+    "Topic": "LEGAL_REGULATORY_FRAMEWORK",
+    "Question": "The National Electricity Plan is notified",
+    "Option1": "once a year",
+    "Option2": "once in two years",
+    "Option3": "once in five years",
+    "Option4": "as and when felt appropriate",
+    "answer": 3
+  },
+  {
+    "Topic": "LEGAL_REGULATORY_FRAMEWORK",
+    "Question": "Non-compliance of directions issued by RLDCs/ SLDCs evokes a penalty of maximum",
+    "Option1": "Rs. 5 lakhs",
+    "Option2": "Rs. 10 lakhs",
+    "Option3": "Rs. 15 lakhs",
+    "Option4": "Rs. 20 lakhs",
+    "answer": 3
+  },
+  {
+    "Topic": "LEGAL_REGULATORY_FRAMEWORK",
+    "Question": "Which of the following is the main aim of the IE Act 2003 from its Preamble?",
+    "Option1": "Govt.-strengthening",
+    "Option2": "Competition",
+    "Option3": "SEB-centric",
+    "Option4": "job creation",
+    "answer": 2
+  },
+  {
+    "Topic": "LEGAL_REGULATORY_FRAMEWORK",
+    "Question": "Which of the following new organisations has been created under the Act.",
+    "Option1": "SERC",
+    "Option2": "CERC",
+    "Option3": "Appellate Tribunal",
+    "Option4": "Supreme Court",
+    "answer": 3
+  },
+  {
+    "Topic": "LEGAL_REGULATORY_FRAMEWORK",
+    "Question": "Who is entrusted with the framing of Electricity Policy?",
+    "Option1": "State Government",
+    "Option2": "Central Government",
+    "Option3": "SERC",
+    "Option4": "CERC",
+    "answer": 2
+  },
+  {
+    "Topic": "LEGAL_REGULATORY_FRAMEWORK",
+    "Question": "What do you understand by the term,'subordinate legislation'?",
+    "Option1": "By State Legislatures",
+    "Option2": "By Parliament Committees",
+    "Option3": "Govt. Controls",
+    "Option4": "Rules and Regulations",
+    "answer": 4
+  },
+  {
+    "Topic": "LEGAL_REGULATORY_FRAMEWORK",
+    "Question": "Who is to frame the Tariff policy?",
+    "Option1": "Central Govt.",
+    "Option2": "CERC",
+    "Option3": "SERC",
+    "Option4": "State Govt.",
+    "answer": 1
+  },
+  {
+    "Topic": "LEGAL_REGULATORY_FRAMEWORK",
+    "Question": "Who is to lay down the guidelines for Competitive bidding for tariff purposes?",
+    "Option1": "State Govt",
+    "Option2": "Central Govt",
+    "Option3": "SERC",
+    "Option4": "CERC",
+    "answer": 2
+  },
+  {
+    "Topic": "LEGAL_REGULATORY_FRAMEWORK",
+    "Question": "Which of the following is the appropriate reason for private sector participation under the Act?",
+    "Option1": "Replace Public Sector",
+    "Option2": "Replace the foreign Companies",
+    "Option3": "Complement public sector",
+    "Option4": "Efficiency",
+    "answer": 3
+  },
+  {
+    "Topic": "LEGAL_REGULATORY_FRAMEWORK",
+    "Question": "Which of the following lays down the Performance Standards in the Power Sector?",
+    "Option1": "State Govt",
+    "Option2": "Central Govt",
+    "Option3": "SERC",
+    "Option4": "CERC",
+    "answer": 3
+  },
+  {
+    "Topic": "LEGAL_REGULATORY_FRAMEWORK",
+    "Question": "Who has powers to nominate a state arbitrator under the Act?",
+    "Option1": "High Court",
+    "Option2": "State Govt.",
+    "Option3": "SERC",
+    "Option4": "CERC",
+    "answer": 3
+  },
+  {
+    "Topic": "LEGAL_REGULATORY_FRAMEWORK",
+    "Question": "Who appoints  Electricity Ombudsman in a State?",
+    "Option1": "State Govt",
+    "Option2": "Central Govt",
+    "Option3": "SERC",
+    "Option4": "CERC",
+    "answer": 3
+  },
+  {
+    "Topic": "EC_ACT",
+    "Question": "The nodal agency at the centre for implementing the EC act is",
+    "Option1": "CEA",
+    "Option2": "SDA",
+    "Option3": "BEE",
+    "Option4": "Ministry of Power",
+    "answer": 3
+  },
+  {
+    "Topic": "EC_ACT",
+    "Question": "For Successful implementation of  National Mission for Enhanced Energy Efficiency, which of the following initiatives are taken?",
+    "Option1": "Perform , Achieve & Trade, Market Transformation for Energy Efficiency",
+    "Option2": "Energy Efficiency Financing Platform",
+    "Option3": "Framework For Energy Efficient Economic Development",
+    "Option4": "All of these",
+    "answer": 4
+  },
+  {
+    "Topic": "EC_ACT",
+    "Question": "Purpose of Demand Side Management (DSM) in Agriculture is",
+    "Option1": "Reducing the overall power consumption",
+    "Option2": "Improving efficiencies of ground water extraction",
+    "Option3": "Reducing the subsidy burden of the states without sacrificing the service obligation to the sector",
+    "Option4": "All of these",
+    "answer": 4
+  },
+  {
+    "Topic": "EC_ACT",
+    "Question": "The National Policy for stand alone systems for rural areas shall be notified by",
+    "Option1": "Central Government",
+    "Option2": "CERC",
+    "Option3": "CEA",
+    "Option4": "Central Government in consultation with the state governments",
+    "answer": 4
+  },
+  {
+    "Topic": "EC_ACT",
+    "Question": "The EC Act 2001 is applicabe to",
+    "Option1": "Whole India",
+    "Option2": "Whole India Except North Eastern States",
+    "Option3": "Whole India Except Jammu & Kashmir",
+    "Option4": "Whole India Except Union Teritories",
+    "answer": 3
+  },
+  {
+    "Topic": "EC_ACT",
+    "Question": "As per Energy Conservation Act, 2001 Cycle means",
+    "Option1": "the period of three years available to a designated consumer to\ncomply with the energy consumption norms and standards",
+    "Option2": "the period of five years available to a designated consumer to\ncomply with the energy consumption norms and standards",
+    "Option3": "the period of two years available to a designated consumer to\ncomply with the energy consumption norms and standards",
+    "Option4": "the period of ten years available to a designated consumer to\ncomply with the energy consumption norms and standards",
+    "answer": 1
+  },
+  {
+    "Topic": "EC_ACT",
+    "Question": "PAT means in EC Act 2001 is",
+    "Option1": "Profit After Trade",
+    "Option2": "Perfom After Trade",
+    "Option3": "Perfrom Achieve Trade",
+    "Option4": "Profit Achieve Trade",
+    "answer": 3
+  },
+  {
+    "Topic": "EC_ACT",
+    "Question": "The number of ammendments made to the EC Act till date is",
+    "Option1": 1,
+    "Option2": 2,
+    "Option3": 3,
+    "Option4": 4,
+    "answer": 1
+  },
+  {
+    "Topic": "EC_ACT",
+    "Question": "Who will specify the qualifications for the accredited energy auditors;",
+    "Option1": "Central Government",
+    "Option2": "CERC",
+    "Option3": "CEA",
+    "Option4": "BEE",
+    "answer": 4
+  },
+  {
+    "Topic": "EC_ACT",
+    "Question": "The powers to alter the list of Energy Intensive Industries specified in the Schedule is with",
+    "Option1": "BEE",
+    "Option2": "Central Government",
+    "Option3": "CEA",
+    "Option4": "CERC",
+    "answer": 2
+  },
+  {
+    "Topic": "TTC_RELIABILITY",
+    "Question": "500 MW power is being transferred from Area A to Area B limited by the transfer capabilty. A contingency in the system leads to voltages falling below 380 kV on the 400 kV bus. Which of the statements is true?",
+    "Option1": "The ATC for Area A to Area B would become more than 500 MW.",
+    "Option2": "1000 MW can be easily transferred from Area B to Area A.",
+    "Option3": "The ATC from Area A to Area B would become less than 500 MW.",
+    "Option4": "500 MW can be easily scheduled from Area A to Area B.",
+    "answer": 3
+  },
+  {
+    "Topic": "TTC_RELIABILITY",
+    "Question": "Area A is connected to areas B and C. There is no interconnection between Area B and C. The TTC from Area B to A is 1200 MW while the TTC from Area C to Area A is 700 MW. The simultaneous import capability of Area A would be:",
+    "Option1": "greater than 1900 MW",
+    "Option2": "500 MW",
+    "Option3": "Could be less than or equal to 1900 MW.",
+    "Option4": "1900 MW",
+    "answer": 3
+  },
+  {
+    "Topic": "TTC_RELIABILITY",
+    "Question": "The different limits to be considered while evaluating TTC are:",
+    "Option1": "Stability, voltage and thermal limits",
+    "Option2": "Sag, active power and reactive power",
+    "Option3": "temperature, pressure and relative humidity",
+    "Option4": "MVA",
+    "answer": 1
+  },
+  {
+    "Topic": "TTC_RELIABILITY",
+    "Question": "Which relationship between transfer capability and transmission capacity is generally the case?",
+    "Option1": "Transfer capability is greater than transmission capacity",
+    "Option2": "Both are equal to each other",
+    "Option3": "Transfer capability is less than transmission capacity",
+    "Option4": "None of these",
+    "answer": 3
+  },
+  {
+    "Topic": "TTC_RELIABILITY",
+    "Question": "Two 315 MVA, 400/220 kV interconnecting transformers are operating in parallel with the interconnected system. The transfer capability in the 400 kV to 220 kV direction is 400 MW. The transfer capability in the 220 kV to 400 kV direction would be:",
+    "Option1": "400 MW",
+    "Option2": "Would have to be worked out from simulation studies",
+    "Option3": "630 MW",
+    "Option4": "Zero",
+    "answer": 2
+  },
+  {
+    "Topic": "TTC_RELIABILITY",
+    "Question": "What would happen to the Surge Impedance Loading (SIL) of a twin Moose 400 kV line if its length is doubled?",
+    "Option1": "No change",
+    "Option2": "Doubles",
+    "Option3": "Becomes half",
+    "Option4": "Becomes four times.",
+    "answer": 1
+  },
+  {
+    "Topic": "TTC_RELIABILITY",
+    "Question": "The system operator gets a request for 700 MW open access contracts from System A to System B. The ATC between these two systems is however only 500 MW. What will the operator do?",
+    "Option1": "Increase the TTC by 200 MW and approve all the open access requests.",
+    "Option2": "Reduce reliability margin by 200 MW and approve all the open access requests.",
+    "Option3": "Approve all the transactions and curtail in real time in case of a congestion",
+    "Option4": "Issue a curtailed approval based on ATC.",
+    "answer": 4
+  },
+  {
+    "Topic": "TTC_RELIABILITY",
+    "Question": "Reliability is a combination of",
+    "Option1": "Adequacy and security",
+    "Option2": "Availability and dependability",
+    "Option3": "Mean Time To Repair and Mean Time Between Failures",
+    "Option4": "availability and optimal loading",
+    "answer": 1
+  },
+  {
+    "Topic": "TTC_RELIABILITY",
+    "Question": "Outage hours to customer makes sense as long as",
+    "Option1": "VOLL is less than cost of providing peaking power",
+    "Option2": "Consumer protection laws are lax",
+    "Option3": "Does not make sense at all",
+    "Option4": "None of the above",
+    "answer": 1
+  },
+  {
+    "Topic": "TTC_RELIABILITY",
+    "Question": "St. Clair curve gives the relationship between",
+    "Option1": "Line loadability in terms of SIL versus line length",
+    "Option2": "Line loading and voltage drop",
+    "Option3": "Line loading and reactive power",
+    "Option4": "None of the above",
+    "answer": 1
+  },
+  {
+    "Topic": "TTC_RELIABILITY",
+    "Question": "TRM is basically required for the purpose of",
+    "Option1": "Mitigating uncertainty in system conditions",
+    "Option2": "Help the system operator in real time",
+    "Option3": "Barriers to free trade and restraint on traders",
+    "Option4": "None of the above",
+    "answer": 1
+  }
+];
